5046944060f625d03fce2ea09152cf0780c900ec
kgneteu <77528950+kgneteu@users.noreply.github.com>
2021-11-08 15:22:22 +0100

    gh pages

--- src/app/sticky-notes/local-storage.service.ts
+++ src/app/sticky-notes/local-storage.service.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 
 @Injectable({providedIn: 'root'})
-export class LocalStorageService {
+  export class LocalStorageService {
   //
   // private maxSent = 5;
   // private lorem = new LoremIpsum({
